11d6f8e9e9735c3ebc0b34692951247c2365cf67
rmfranken <robin.franken.mail@gmail.com>
2026-05-19 11:57:12 +0200

    feat: add nix support

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "text-contextifyer dev environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            python311
+            poetry
+            docker
+          ];
+
+          shellHook = ''
+            export POETRY_VIRTUALENVS_IN_PROJECT=true
+            echo "Run 'poetry install' to set up the project."
+          '';
+        };
+      });
+}
